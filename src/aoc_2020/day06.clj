4663d012c7ca9e9d131f38d5f102c750eea0fd0e
Sable Seyler <sable@seyleri.us>
2020-12-05 23:54:57 -0600

    Switch to simpler str/replace solution

--- src/aoc_2020/day06.clj
+++ src/aoc_2020/day06.clj
@@ -1,30 +1,21 @@
 (ns aoc-2020.day06
   (:require [clojure.java.io :as io]
-            [instaparse.core :as insta]
-            [instaparse.combinators :as c]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.string :as str]))
 
 (def input (slurp (io/reader (io/resource "input06"))))
 
-(def customs-grammar
-  (insta/parser
-   {:answers (c/cat (c/nt :group) (c/star (c/cat (c/hide (c/nt :group-sep)) (c/nt :group))) (c/hide (c/nt :eof)))
-    :group (c/cat (c/nt :person) (c/star (c/cat (c/hide (c/nt :person-sep)) (c/nt :person))))
-    :person (c/plus (c/nt :letter))
-    :letter (c/hide-tag (c/regexp "[a-z]"))
-    :group-sep (c/hide-tag (c/string "\n\n"))
-    :person-sep (c/hide-tag (c/string "\n"))
-    :eof (c/hide-tag (c/cat (c/opt (c/string "\n")) (c/regexp "$")))}
-   :start :answers))
+(defn all-group-answers [answers]
+  (->> (str/split answers #"\n\n")
+       (map #(str/replace % #"\n" ""))
+       (map set)
+       (map count)
+       (apply +)))
 
-(def answer-transform
-  {:group set/intersection
-   :person (fn [& answers] (set answers))
-   :answers vector})
-
-(defn sum-answer-counts [answers]
-  (->> answers
-       customs-grammar
-       (insta/transform answer-transform)
+(defn agreed-group-answers [answers]
+  (->> (str/split answers #"\n\n")
+       (map #(str/split % #"\n"))
+       (map #(map set %))
+       (map #(apply set/intersection %))
        (map count)
        (apply +)))
